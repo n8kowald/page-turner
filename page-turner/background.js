2bfe4c806303f0a2457709669660a1262b2efd45
Nathan Kowald <nathankowald@gmail.com>
2012-06-10 20:51:54 +0100

    fix messed up icon on page refresh, roll back

--- page-turner/background.js
+++ page-turner/background.js
@@ -1,11 +1,13 @@
 var page_icon = new Array();
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
         var tab_id = sender.tab.id;
-        chrome.browserAction.setIcon({path: 'icons/' + request.icon, tabId: tab_id});
+        chrome.browserAction.setIcon({path: 'icons/' + request.icon});
         page_icon[tab_id] = request.icon;
 });
 chrome.tabs.onActivated.addListener(function(tab) {
     if (typeof page_icon[tab.tabId] != 'undefined') {
         chrome.browserAction.setIcon({path: 'icons/' + page_icon[tab.tabId]});
+    } else {
+        chrome.browserAction.setIcon({path: 'icons/icon-inactive.png'});
     }
 });
